5e05583d10104366e953ee987dbb4e65d3d9d8ac
Emil K Kurian <emkurian97@gmail.com>
2018-03-30 20:13:31 +0530

    Second upload

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/adc4.0jp/';
+$config['base_url'] = 'http://35.196.227.67/Dekbhal/';
 
 /*
 |--------------------------------------------------------------------------
